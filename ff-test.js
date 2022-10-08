5f14468278cc13a917c2c251632e3fd9e80ca831
flipflapflipflap <112861302+flipflapflipflap@users.noreply.github.com>
2022-10-08 03:34:02 -0400

    Update ff-test.js

--- ff-test.js
+++ ff-test.js
@@ -2328,7 +2328,7 @@ function countdown (element) {
 			if (31 - dayoffset > Day) {
 						fieldNameElement.innerHTML = "Countdown to (next) October:";
 						cdtext = 11 + ' : ' + Day + ' : ' + Hour + ' : ' + Minute + ' : ' + Seconds;
-					} else if ((31 - dayoffset == Day) && Hour < (24 - 7)) {
+					} else if ((31 - dayoffset == Day) && Hour < (24 - 8)) {
 						cdtext = "THE TIME HAS COME";
 					} else {
 							temp = dayoffset - day;
